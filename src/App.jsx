7a9669b8778fd116b13329b416909740be468dde
Salimov-dev <salimov.rent@mail.ru>
2023-06-06 21:12:58 +0300

    настроил базовый роутинг

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+// libraries
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+// components
+import Posts from "./components/page/posts";
+import AboutDeveloper from "./components/page/about-developer";
+import AboutUser from "./components/page/about-user";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Posts />} />
+          <Route path="/developer" element={<AboutDeveloper />} />
+          <Route path="/user/:userId?" element={<AboutUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
